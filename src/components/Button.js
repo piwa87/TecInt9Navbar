af659f299fa5856f48ff2596ff1f3e33f531eb34
LKrivaa <90308251+LKrivaa@users.noreply.github.com>
2021-11-08 11:07:52 +0100

    Button component

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+background-color: #8A9828;
+height: 36px;
+width: 115px;
+font: "STIXVariants";
+font-size: 18;
+text-align: center;
+`;
+
+export const CancelButton = styled.button`
+background-color: #591420;
+height: 36px;
+width: 115px;
+`;
+
+export const ButtonText = styled.label`
+font: "STIXVariants";
+font-size: 18;
+text-align: center;
+color: white;
+`
+export const ButtonDisabled = styled.button`
+background-color: grey;
+height: 36px;
+width: 115px;
+`;
+

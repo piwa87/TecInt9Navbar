b73b9c65a233f0c6fbe734b5afe32e4efb2e893e
Amanda <mujo@itu.dk>
2021-11-08 11:28:46 +0100

    Excursion updated

--- src/pages/excursions.js
+++ src/pages/excursions.js
@@ -1,13 +1,87 @@
-import React from "react"
+import React from 'react';
+import Cabin from '../images/Cabin.jpg'; 
+import '../App.css';
+
+
 
 const Excursions = () => {
-    return (
-        <div>
-            <h3>Excursion page!
-            </h3>
-        </div>
-    )
-}
+return( 
+	<div className='Container'
+	style={{
+		backgroundColor: '#B4BF5E',
+		marginTop: '-21px',
+		marginLeft: '282px',
+		marginRight: '282px',
+		height: '180vh'
+	}}>
+	<h1 className='about_h1'style={{
+              textAlign: "center",
+			  paddingTop:50
+            }}>Excursion 2021</h1>
+
+
+	<div className='first-collumn-image'>	
+	<img src= {Cabin} 
+	alt="sweden"
+	style={{
+	justifyContent:"center",
+	position: "absolute",
+	width: "900px",
+	height: "390px",
+	left: "402px",
+	top: "257px",
+	alignItems:'center'
+	}}
+	/>
+
+	</div>
+	<div className='info'>
+	<p 
+	style={{
+              fontWeight: 300,
+			  fontFamily: "STIXVariants",
+			  paddingTop:500,
+			  textAlign: "center",
+			  fontSize: 28
+            }}
+			>
+			Friday to sunday<strong> June 7-9 2021</strong><br />Location: <strong>Vilshult in Småland, SWEDEN</strong><br />Price: Adult 950 kr.<br />  Child 475 kr.             
+                
+			
+	</p>
+	</div>
+	<div className='line'>
+	<hr
+	style={{
+		backgroundColor:"black",
+        width: 790,
+		height: 5,
+        marginLeft: "auto",
+        marginRight: "auto",
+		border: 0
+		}}
+		/>
+
+	</div>	
+
+	<div className='join'>
+	<p
+	style={{
+		fontWeight: 300,
+		fontFamily: "STIXVariants",
+		paddingTop: 15,
+		textAlign: "center",
+		fontSize: 28,
+		position: "static",
+		
+	  }}
+	  >	
+		Join this year’s excursion, there is room for 150 people. So sign up before your colleague </p>
+	</div>
+	</div>
+
+);
+};
 
 
 export default Excursions

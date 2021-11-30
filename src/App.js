f882ec2f17b346843f2e86aca4f567aa5108fd43
Piotrek Wasilewski <piotrek@dunesite.com>
2021-11-30 17:05:33 +0100

    Fixed file names

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import './App.css'
+import './App.css'
 
 // Importing Navbar component:
 import Navbar from "./components/Navbar"

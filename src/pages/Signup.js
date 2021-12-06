1bcfa8fcc79c70371e8e2242983fc744fd020a20
Amanda <mujo@itu.dk>
2021-12-06 11:51:14 +0100

    car sign up window

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -33,7 +33,6 @@ export default function CreateSignUp() {
     function numberOfGuestsChange(e) {
         setNumberOfGuests(e.target.value)
     }
-
     function clearInput() {
         setFullName("")
         setAddress("")

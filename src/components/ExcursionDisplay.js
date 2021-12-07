90409a6385173d7a381bc5f6dd81fa38aeda9c4c
Piotrek Wasilewski <piotrek@dunesite.com>
2021-12-07 15:47:43 +0100

    more changes

--- src/components/ExcursionDisplay.js
+++ src/components/ExcursionDisplay.js
@@ -1,23 +1,28 @@
+import { useNavigate } from "react-router";
 import "../tempData/excursionDetails";
 import { ButtonSignup, ButtonTextSignUp } from './Button';
 import "./Ex.css";
 
 
-
 export default function ExcursionDisplay(props) {
-    return( 
-        <div className = "ex-info">
-        <img alt="" className="ex-image" src={props.par.img}/> 
-        <p>Duration: <b>{props.par.duration} {props.par.date}</b> </p> 
-        <p>Location: <b>{props.par.location}</b></p>
-        <p>Adult: <b>{props.par.priceAdult} Kr.</b></p>
-        <p>Child: <b>{props.par.priceAdult/2} Kr.</b></p>
-  
-        <hr/>
-        <p>Join this year’s excursion, there is room for {props.par.maxCapacity} people. We look forward to seing you!</p>
-        <p className="ex-button"><ButtonSignup><ButtonTextSignUp>Sign up</ButtonTextSignUp></ButtonSignup></p>
 
+    const navigate = useNavigate();
 
+    return (
+        <div className="ex-info">
+            <img alt="" className="ex-image" src={props.par.img} />
+            <p>Duration: <b>{props.par.duration} {props.par.date}</b> </p>
+            <p>Location: <b>{props.par.location}</b></p>
+            <p>Adult: <b>{props.par.priceAdult} Kr.</b></p>
+            <p>Child: <b>{props.par.priceAdult / 2} Kr.</b></p>
+
+            <hr />
+            <p>Join this year’s excursion, there is room for {props.par.maxCapacity} people. We look forWard to seeing you!</p>
+            <p>
+                <ButtonSignup className="ex-button" onClick={() => navigate("/signup")}>
+                    <ButtonTextSignUp>Sign up</ButtonTextSignUp>
+                </ButtonSignup>
+            </p>
         </div>
     )
 }
